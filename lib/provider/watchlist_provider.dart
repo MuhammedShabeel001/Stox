850e6a35eb053a23eaa9a3aa76c70833903e9b55
MuhammedShabeel001 <mshabeel999@gmail.com>
2024-10-17 09:04:42 +0530

    watchlist provider

---
+++ lib/provider/watchlist_provider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+import '../model/stocks_model.dart';
+
+class WatchlistProvider with ChangeNotifier {
+  final Box<StockModel> _watchlistBox = Hive.box<StockModel>('watchlist');
+
+  List<StockModel> _watchlist = [];
+  List<bool> _expandedStates = [];
+
+  List<StockModel> get watchlist => _watchlist;
+
+  bool isExpanded(int index) =>
+      _expandedStates.length > index ? _expandedStates[index] : false;
+
+  WatchlistProvider() {
+    loadWatchlist();
+  }
+
+  void loadWatchlist() {
+    _watchlist = _watchlistBox.values.toList();
+    _expandedStates = List<bool>.filled(_watchlist.length, false);
+    notifyListeners();
+  }
+
+  void addStock(StockModel stock) {
+    _watchlistBox.put(stock.symbol, stock);
+    _watchlist = _watchlistBox.values.toList();
+    _expandedStates.add(false);
+    notifyListeners();
+  }
+
+  void removeStock(String symbol) {
+    final index = _watchlist.indexWhere((stock) => stock.symbol == symbol);
+    if (index != -1) {
+      _watchlistBox.delete(symbol);
+      _watchlist.removeAt(index);
+      _expandedStates.removeAt(index);
+      notifyListeners();
+    }
+  }
+
+  void toggleExpansion(int index) {
+    if (index < _expandedStates.length) {
+      _expandedStates[index] = !_expandedStates[index];
+      notifyListeners();
+    }
+  }
+}
