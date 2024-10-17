65e03eb7ee68d4163169fcfe5812bccd68ed10bd
MuhammedShabeel001 <mshabeel999@gmail.com>
2024-10-17 09:01:49 +0530

    stock provider

--- lib/provider/stock_provider.dart
+++ lib/provider/stock_provider.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 import '../model/stocks_model.dart';
+import '../services/stock_services.dart';
 
-class StockProvider with ChangeNotifier{
-  List<StockModel>  _stock =[];
-  final StockService _stockService =
-  
+class StockProvider with ChangeNotifier {
+  List<StockModel> _stock = [];
+  final StockService _stockService = StockService();
+
+  List<StockModel> get stocks => _stock;
+
+  Future<void> searchStocks(String query) async {
+    if (query.isNotEmpty) {
+      _stock = await _stockService.searchStocks(query);
+      notifyListeners();
+    } else {
+      _stock = [];
+      notifyListeners();
+    }
+  }
 }
